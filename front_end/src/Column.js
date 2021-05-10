3a4505c88be9fc67cd927034f67b6dd72e840f02
James Ware <jazz642@gmail.com>
2021-05-09 23:06:11 -0400

    Finished tutorial, now I am going to try and use this framework with my cards for the trello clone

--- front_end/src/Column.js
+++ front_end/src/Column.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Task from './Task';
-import styled from 'styled-components';
-import { Droppable } from 'react-beautiful-dnd';
+import styled, { ThemeProvider } from 'styled-components';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
+  background-color: white;
   border-radius: 2px;
+  width: 220px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h3`
@@ -14,25 +18,40 @@ const Title = styled.h3`
 `;
 const TaskList = styled.div`
   padding: 8px;
+  flex-grow: 1;
+  min-height: 100px;
 `;
 
 export default class Column extends React.Component {
   render() {
     return (
-      <Container>
-        <Title>{this.props.column.title}</Title>
-        <Droppable droppableId={this.props.column.id}>
-          {provided => (
-            <TaskList ref={provided.innerRef} {...provided.droppableProps}>
-              {this.props.tasks.map((task, index) => (
-                <Task key={task.id} task={task} index={index} />
-              ))}
-              {provided.placeholder}
-            </TaskList>
-          )}
+      <Draggable
+        draggableId={this.props.column.id} index={this.props.index}
+      >
+        {provided => (
+          <Container
+            {...provided.draggableProps} ref={provided.innerRef}
+          >
+            <Title {...provided.dragHandleProps}>
+              {this.props.column.title}
+            </Title>
+            <Droppable droppableId={this.props.column.id} type="task">
+              {(provided, snapshot) => (
+                <TaskList
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  {this.props.tasks.map((task, index) => (
+                    <Task key={task.id} task={task} index={index} />
+                  ))}
+                  {provided.placeholder}
+                </TaskList>
+              )}
 
-        </Droppable>
-      </Container>
+            </Droppable>
+          </Container>
+        )}
+      </Draggable>
     )
   }
 }
